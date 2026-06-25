0beb809533b6bac8164df4c0b9a60ad9e2c05c47
sahilbind05 <bindsahil9@gmail.com>
2026-06-25 23:58:15 +0530

    Time: 1 ms (99.98%), Space: 42.8 MB (11.75%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int reverse(int x) {
+        int reversed = 0;
+
+        while (x != 0) {
+            int digit = x % 10;
+            x /= 10;
+
+            // Check overflow before multiplying
+            if (reversed > Integer.MAX_VALUE / 10 || 
+                reversed < Integer.MIN_VALUE / 10) {
+                return 0;
+            }
+
+            reversed = reversed * 10 + digit;
+        }
+
+        return reversed;
+    }
+}
