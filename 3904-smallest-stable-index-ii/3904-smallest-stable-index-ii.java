0f5463a11ae7b927c04ad041961c6e7b597133f9
sahilbind05 <bindsahil9@gmail.com>
2026-09-05 23:02:13 +0530

    Time: 4 ms (88.55%), Space: 133.3 MB (20.99%) - LeetHub

--- 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.java
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.java
@@ -5,15 +5,17 @@ class Solution {
 
         min[n-1]=nums[n-1];
 
+        // Smaalest value from every idx
         for(int i = n-2; i>=0; i--){
             min[i]= Math.min(min[i+1],nums[i]);
         }
 
         int max = 0;
 
+        //largest values
         for(int j =0; j<n; j++){
             max = Math.max(max,nums[j]);
-
+            
             int temp = max-min[j];
 
             if(temp <= k){
